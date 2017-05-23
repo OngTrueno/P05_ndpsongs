62593fcb57876c3804d0b9df9a2cd00fbacbce01
15017470 <15017470@myrp.edu.sg>
2017-05-23 14:37:30 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a15017470/p05_ndpsongs/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.a15017470.p05_ndpsongs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
